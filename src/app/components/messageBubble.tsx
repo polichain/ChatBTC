7d622d06ca6cb77c90a1944c691d080c6b0e1b6d
Turina <112657934+Turina7@users.noreply.github.com>
2024-10-28 15:40:24 -0300

    Marckdown (#1)
    * commit na main
    
    * Add markdown on Bubble component

--- src/app/components/messageBubble.tsx
+++ src/app/components/messageBubble.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 interface MessageProps {
   text: string;
@@ -8,32 +9,32 @@ interface MessageProps {
 
 const MessageBubble: React.FC<MessageProps> = ({ text, timestamp, isSentByUser }) => {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: isSentByUser ? 'flex-end' : 'flex-start',
-      padding: '8px'
-    }}>
-      <div style={{
-        maxWidth: '60%',
-        padding: '10px',
-        paddingBottom: '20px',
-        backgroundColor: isSentByUser ? '#fff' : '',
-        color: isSentByUser ? '#000' : '#fff',
-        borderRadius: '8px',
-        boxShadow: isSentByUser ? '0 1px 2px rgba(0, 0, 0, 0.2)' : '',
-        fontSize: '14px',
-        lineHeight: '20px',
-        position: 'relative'
-      }}>
-        <span>{text}</span>
+    <div style={{ display: 'flex', justifyContent: isSentByUser ? 'flex-end' : 'flex-start', padding: '8px' }}>
+      <div
+        style={{
+          maxWidth: '60%',
+          padding: '10px',
+          paddingBottom: '20px',
+          backgroundColor: isSentByUser ? '#fff' : '#333', // Darker background for received messages
+          color: isSentByUser ? '#000' : '#fff',
+          borderRadius: '8px',
+          boxShadow: isSentByUser ? '0 1px 2px rgba(0, 0, 0, 0.2)' : '',
+          fontSize: '14px',
+          lineHeight: '20px',
+          position: 'relative',
+        }}
+      >
+        <ReactMarkdown>{text}</ReactMarkdown>
         {timestamp && (
-          <span style={{
-            fontSize: '10px',
-            color: '#888',
-            position: 'absolute',
-            bottom: '5px',
-            right: '10px'
-          }}>
+          <span
+            style={{
+              fontSize: '10px',
+              color: '#888',
+              position: 'absolute',
+              bottom: '5px',
+              right: '10px',
+            }}
+          >
             {timestamp}
           </span>
         )}
